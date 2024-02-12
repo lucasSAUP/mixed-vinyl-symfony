7333715deab6cfe2afc5d1e2922279e035b75c1b
lucasSAUP <lsarachu@up-spain.com>
2024-02-12 19:19:52 +0100

    Added some Twiggy goodness

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -1,15 +1,30 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\u;
 
-class VinylController {
+class VinylController extends AbstractController
+ {
 
-    #[Route(path:"", name:"")]
+    #[Route(path:"/")]
     public function homepage() : Response {
-        return new Response('Title: PB and Jams');
+
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+        
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks,
+        ]);
     }
 
     // {slug} o la palabra que queramos será la palabra tecnica para designar un "Nombre seguro para la url y que lo conozca"
